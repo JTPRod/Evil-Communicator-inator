daaa05625cee77ee0ae2f9805c8dbf99599250f5
JTPRod <jarodriguez@student.neumont.edu>
2025-11-18 19:06:54 -0700

    Add basic servlet backend
    POJO for users/messages, REST hooks, prepped project for mongo implementation

---
+++ backend/EvilCommunicatorInator/src/main/java/evil/doofenshmirtz/evilcommunicatorinator/Controller/MessageRestController.java
@@ -0,0 +1,108 @@
+package evil.doofenshmirtz.evilcommunicatorinator.Controller;
+
+import java.util.List;
+
+import evil.doofenshmirtz.evilcommunicatorinator.Models.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageRestController {
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public String create(@RequestBody Message message) {
+
+        switch (Settings.dbStatus) {
+            case ARRAYLIST:
+                return MessageRestDataArrayList.add(message);
+
+            case SQL:
+                return null;
+
+            case JPA:
+                return null;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<Message> getAll() {
+        switch (Settings.dbStatus) {
+            case ARRAYLIST:
+                return MessageRestDataArrayList.getAll();
+
+            case SQL:
+                return null;
+
+            case JPA:
+                return null;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Message findById(@PathVariable int id) {
+        switch (Settings.dbStatus) {
+            case ARRAYLIST:
+                return MessageRestDataArrayList.getById(id);
+
+            case SQL:
+                return null;
+
+            case JPA:
+                return null;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.PUT)
+    public Message update(@RequestBody Message message) {
+        switch (Settings.dbStatus) {
+            case ARRAYLIST:
+                return MessageRestDataArrayList.update(message);
+
+            case SQL:
+                return null;
+
+            case JPA:
+                return null;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public String deleteById(@PathVariable int id) {
+        switch (Settings.dbStatus) {
+            case ARRAYLIST:
+                return MessageRestDataArrayList.deleteById(id);
+
+            case SQL:
+                return null;
+
+            case JPA:
+                return null;
+
+            default:
+                return null;
+        }
+
+    }
+
+}
---
+++ backend/EvilCommunicatorInator/src/main/java/evil/doofenshmirtz/evilcommunicatorinator/Models/User.java
@@ -0,0 +1,49 @@
+package evil.doofenshmirtz.evilcommunicatorinator.Models;
+
+public class User {
+    private String username;
+    private String password;
+    private int userId;
+    private String bio;
+
+    public User() {}
+
+    public User(String username, String password, int userId, String bio) {
+        this.username = username;
+        this.password = password;
+        this.userId = userId;
+        this.bio = bio;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
