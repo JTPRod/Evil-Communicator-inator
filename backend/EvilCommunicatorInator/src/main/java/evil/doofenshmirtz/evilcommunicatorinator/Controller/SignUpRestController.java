d1ea241649abd0a10ae2c0e1ca948bf9ad541fbd
JTPRod <jarodriguez@student.neumont.edu>
2025-11-19 19:14:02 -0700

    separate login from sign up (#2)
    * separate login from sign up
    
    make duplicate classes for login and sign up, instead of just 'user'
    
    * change mapping
    
    login and user were mapped to /user, changed accordingly

--- backend/EvilCommunicatorInator/src/main/java/evil/doofenshmirtz/evilcommunicatorinator/Controller/SignUpRestController.java
+++ backend/EvilCommunicatorInator/src/main/java/evil/doofenshmirtz/evilcommunicatorinator/Controller/SignUpRestController.java
@@ -1,25 +1,20 @@
 package evil.doofenshmirtz.evilcommunicatorinator.Controller;
 
+import evil.doofenshmirtz.evilcommunicatorinator.Models.SignUp;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-import evil.doofenshmirtz.evilcommunicatorinator.Models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-@RequestMapping("/user")
-public class UserRestController {
+@RequestMapping("/signup")
+public class SignUpRestController {
 
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public String create(@RequestBody User user) {
+    public String create(@RequestBody SignUp signUp) {
 
         switch (Settings.dbStatus) {
             case ARRAYLIST:
-                return UserRestDataArrayList.add(user);
+                return SignUpRestDataArrayList.add(signUp);
 
             case SQL:
                 return null;
@@ -34,10 +29,10 @@ public class UserRestController {
     }
 
     @RequestMapping(path = "", method = RequestMethod.GET)
-    public List<User> getAll() {
+    public List<SignUp> getAll() {
         switch (Settings.dbStatus) {
             case ARRAYLIST:
-                return UserRestDataArrayList.getAll();
+                return SignUpRestDataArrayList.getAll();
 
             case SQL:
                 return null;
@@ -52,10 +47,10 @@ public class UserRestController {
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public User findById(@PathVariable int id) {
+    public SignUp findById(@PathVariable int id) {
         switch (Settings.dbStatus) {
             case ARRAYLIST:
-                return UserRestDataArrayList.getById(id);
+                return SignUpRestDataArrayList.getById(id);
 
             case SQL:
                 return null;
@@ -70,10 +65,10 @@ public class UserRestController {
     }
 
     @RequestMapping(path = "", method = RequestMethod.PUT)
-    public User update(@RequestBody User user) {
+    public SignUp update(@RequestBody SignUp signup) {
         switch (Settings.dbStatus) {
             case ARRAYLIST:
-                return UserRestDataArrayList.update(user);
+                return SignUpRestDataArrayList.update(signup);
 
             case SQL:
                 return null;
@@ -91,7 +86,7 @@ public class UserRestController {
     public String deleteById(@PathVariable int id) {
         switch (Settings.dbStatus) {
             case ARRAYLIST:
-                return UserRestDataArrayList.deleteById(id);
+                return SignUpRestDataArrayList.deleteById(id);
 
             case SQL:
                 return null;
