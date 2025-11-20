03179e75c732c206219c6bba4f6c5a35c37c2018
JTPRod <jarodriguez@student.neumont.edu>
2025-11-19 19:13:17 -0700

    change mapping
    login and user were mapped to /user, changed accordingly

--- backend/EvilCommunicatorInator/src/main/java/evil/doofenshmirtz/evilcommunicatorinator/Controller/SignUpRestController.java
+++ backend/EvilCommunicatorInator/src/main/java/evil/doofenshmirtz/evilcommunicatorinator/Controller/SignUpRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/signup")
 public class SignUpRestController {
 
     @RequestMapping(path = "", method = RequestMethod.POST)
